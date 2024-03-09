58da0875005e056f1aea701b9e0b92707d66bedc
Sravani <sravaniperumalla9@gmail.com>
2024-03-09 20:44:50 +0530

    fibonacci upto nth term added

--- fibonacci_upto_nth_term.py
+++ fibonacci_upto_nth_term.py
@@ -1,1 +1,16 @@
 num = int(input())
+
+
+def simple_iteration_method(n):
+    a = 0
+    b = 1
+    print(a, end=" ")
+    print(b, end=" ")
+    for i in range(n - 2):
+        c = a + b
+        a = b
+        b = c
+        print(c, end=" ")
+
+
+simple_iteration_method(num)
